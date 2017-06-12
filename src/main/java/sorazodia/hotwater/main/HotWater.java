bd5d58fc09c2a36035b1c27853050fffe5c1967e
SoraZodia <sorazodia@users.noreply.github.com>
2017-06-12 12:36:16 -1000

    Bumped version

--- src/main/java/sorazodia/hotwater/main/HotWater.java
+++ src/main/java/sorazodia/hotwater/main/HotWater.java
@@ -37,7 +37,7 @@ public class HotWater
 {
 	public static final String MODID = "hot_water";
 	public static final String NAME = "Hot Water Mod";
-	public static final String VERSION = "3.0.2";
+	public static final String VERSION = "4.0.0";
 	public static final String GUI_FACTORY = "sorazodia.hotwater.config.ConfigGUIFactory";
 
 	@Mod.Instance
